d12ac485ea33ac5cd20ddcc0a063a2ffff0ca0f2
Tong-Yin <30991120+Tong-Yin@users.noreply.github.com>
2018-03-22 11:25:30 -0500

    restructured directories

--- application.py
+++ application.py
@@ -4,41 +4,38 @@ from prediction import rf_prediction as rf
 from database import ingest as ingest
 from datetime import datetime
 
-# @application.route('/')
-# def index():
-#
-#     """Main view that displays the survey form to predict LCA status
-#
-#     :return: rendered html template
-#
-#     """
-#     #make dataset
-#     #seed db
-#
-#     return render_template('index.html')
+
+@application.route('/')
+def show():
+    """View that displays main page
+
+        Returns:
+            flask-obj: rendered html page
+
+    """
+    return render_template('index.html')
 
 
 @application.route('/predict', methods=['POST'])
 def submit():
-    # example to get user input
+    """View that process a POST with new user input
+
+        Returns:
+            flask-obj: rendered html page
+    """
+
+    # get user input
     data = request.form
+
+    # call random forest to predict result
     result = rf.predict(data)
     prediction = "No"
     if result == 1:
         prediction = "Yes"
-    # save user input to db
+
+    # save user input to db on RDS
     ingest.seed_db_single_record(datetime.utcnow(), data, result)
     return render_template('result.html', result=prediction)
-
-
-@application.route('/')
-def show():
-
-    """View that process a POST with new user input
-
-    :return: redirect to index page
-    """
-    return render_template('index.html')
 
 
 if __name__ == "__main__":
--- lca_app/__init__.py
+++ lca_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from prediction import generate_rf_model as gm
 
 # needed by beanstalk
 application = Flask(__name__)
@@ -10,3 +10,6 @@ application.config.from_pyfile('../config.py', silent=True)
 
 # Initialize the database
 db = SQLAlchemy(application)
+
+
+clf = gm.load_rf_model()
